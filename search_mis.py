f7f7fda20218cce79d16f5458b7a5f1d18fe5844
chugcha <knchugaeva@edu.hse.ru>
2025-04-09 07:41:24 +0500

    Исправлен баг невключения @ и скобок

--- search_mis.py
+++ search_mis.py
@@ -6,16 +6,17 @@ import json
 counter = 0
 dict_id = {}
 speaker_id = str()
-pattern = r'([А-ЯЁа-яё-]+?)\s\[\:\s([А-ЯЁа-яё-]+?)\]\s\[\*\]'
+pattern = r'([A-ЯЁа-яё@\(\)-]+?)\s\[\:\s(.+?)\]\s\[\*\]'
 pat_age = r'\d+?;\d{2}\.\d{2}'
 pat_part = r'.+?\|ruslan-m\|(.+?)\|'
 pat_speech = r'(.+?)\s(\.|\!|\?)'
-files_li = os.listdir(r'C:\Users\Николай\Desktop\ex`,f\1 курс\практика ошибки\переведённые') #папка с файлами .cha
-if not os.path.isdir(r'C:\Users\Николай\Desktop\ex`,f\1 курс\практика ошибки\переведённые'):  #папка с файлами .cha
-    os.mkdir(r'C:\Users\Николай\Desktop\ex`,f\1 курс\практика ошибки\переведённые')  #папка с файлами .cha
-with open(r'C:\Users\Николай\Desktop\ex`,f\1 курс\практика ошибки\mistakes.txt', 'w', encoding='utf-8') as f_mis: #док записи
+files_li = os.listdir(r'C:\Users\Николай\Desktop\ex`,f\1 курс\практика ошибки\переведённые')  # папка с файлами .cha
+if not os.path.isdir(r'C:\Users\Николай\Desktop\ex`,f\1 курс\практика ошибки\переведённые'):  # папка с файлами .cha
+    os.mkdir(r'C:\Users\Николай\Desktop\ex`,f\1 курс\практика ошибки\переведённые')  # папка с файлами .cha
+with open(r'C:\Users\Николай\Desktop\ex`,f\1 курс\практика ошибки\mistakes.txt', 'w',
+          encoding='utf-8') as f_mis:  # док записи
     for f in files_li:
-        file_name = f #имя файла
+        file_name = f  # имя файла
         if '.cha' in f:
             read_f_path = os.path.join(r'C:\Users\Николай\Desktop\ex`,f\1 курс\практика ошибки\переведённые', f)
             with open(read_f_path, 'r', encoding='utf-8') as curr_f:
@@ -25,23 +26,24 @@ with open(r'C:\Users\Николай\Desktop\ex`,f\1 курс\практика о
                         dict_id[re.findall(pat_part, act_id)[0]] = act_id
                         list_age = re.findall(pat_age, line)
                         if list_age:
-                            ch_age = list_age[0] #предположительно, возраст ребёнка
+                            ch_age = list_age[0]  # предположительно, возраст ребёнка
                     if line.startswith('*'):
                         speaker = line[1:5].replace(':', '').replace(' ', '')
-                        speaker_id = dict_id[speaker].strip() # ID говорящего
+                        speaker_id = dict_id[speaker].strip()  # ID говорящего
                         speech = str(line[5:])
                         if '.' in speech:
                             speech_cleaned = speech[1:speech.find('.') + 1]
                         elif '?' in speech:
                             speech_cleaned = speech[1:speech.find('?') + 1]
                         elif '!' in speech:
-                            speech_cleaned = speech[1:speech.find('!') + 1] #остаток строки без цифр
+                            speech_cleaned = speech[1:speech.find('!') + 1]  # остаток строки без цифр
                             speech_cleaned = speech_cleaned.strip()
                         if re.search(pattern, speech_cleaned):
                             for one_word in re.findall(pattern, speech_cleaned):
-                                counter += 1 #подсчёт ошибок, вдруг пригодится
-                                mis_w = one_word[0] #слово с ошибкой
-                                corr_w = one_word[1] #исправленное слово
+                                counter += 1  # подсчёт ошибок, вдруг пригодится
+                                mis_w = one_word[0]  # слово с ошибкой
+                                corr_w = one_word[1]  # исправленное слово
                                 print(f'{file_name}: {ch_age}, {speaker_id}, "{speech_cleaned}", {mis_w} -> {corr_w}')
-                                f_mis.write(f'{file_name}: {ch_age}; {speaker_id}; "{speech_cleaned}"; {mis_w} -> {corr_w}\n') #на CSV у меня сил пока не хватило
+                                f_mis.write(
+                                    f'{file_name}: {ch_age}; {speaker_id}; "{speech_cleaned}"; {mis_w} -> {corr_w}\n')
 print(counter)
